0414fe2304c5d5ba2eb76a0b6b463f70673fa38a
robertolatini31 <robertolatini31@gmail.com>
2022-05-13 12:32:07 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+   <h1 class="text-center"><font-awesome-icon icon="fa-solid fa-user-secret" /></h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style lang="scss">
+@import '@/assets/scss/style.scss';
+
+</style>
