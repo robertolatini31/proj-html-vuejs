588576ea0656d25965d6dd54b9ab080eaab2b12c
robertolatini31 <robertolatini31@gmail.com>
2022-05-16 16:15:59 +0200

    add slider

--- src/components/TestimonialsSectionComponent.vue
+++ src/components/TestimonialsSectionComponent.vue
@@ -4,23 +4,23 @@
           <div class="col py_4 text-center bg_left">
             <div class="text_story">
               <span class="text-uppercase">real stories</span>
-              <p class="py-4">I am free to learn at my own pace, follow<br> my own schedule and choose the subject i<br> want to learn from the syllabus. Great<br> study portal for people like me.</p>
+              <p class="py-4" v-html="Testimonials[ActiveTestimonial].text"></p>
             </div>
             <!-- /.text_story -->
             <div class="testimonial">
-              <img src="@/assets/img/testimonial-avata-02.jpg" alt="avatar-testimonial">
-              <h6 class="pt-4 pb-2 mb-0">Mina Hollace</h6>
-              <span>/ Freelancer</span>
+              <img :src="Testimonials[ActiveTestimonial].avatar">
+              <h6 class="pt-4 pb-2 mb-0 text-capitalize">{{Testimonials[ActiveTestimonial].name}}</h6>
+              <span>{{Testimonials[ActiveTestimonial].work}}</span>
             </div>
             <!-- /.testimonial -->
             <div class="slider_button d-flex flex-column justify-content-center align-items-center">
-              <div class="top_arrow">
+              <div class="top_arrow" @click="NextTestimonial">
                 <img src="@/assets/img/image.png" alt="">
               </div>
               <div class="center_value">
-                <span>1/4</span>
+                <span>{{ActiveTestimonial + 1 + '/4'}}</span>
               </div>
-              <div class="bottom_arrow">
+              <div class="bottom_arrow" @click="PrecedentTestimonial">
                 <img src="@/assets/img/image(1).png" alt="">
               </div>
             </div>
@@ -38,7 +38,54 @@
 
 <script>
 export default {
-name: 'TestimonialsSectionComponent'
+name: 'TestimonialsSectionComponent',
+data () {
+    return {
+        ActiveTestimonial: 0,
+        Testimonials: [
+            {
+                avatar: require('@/assets/img/testimonial-avata-02.jpg'),
+                text: 'I am free to learn at my own pace, follow<br> my own schedule and choose the subject i<br> want to learn from the syllabus. Great<br> study portal for people like me.',
+                name: 'mina hollace',
+                work: '/ Freelancer'
+            },
+            {
+                avatar: require('@/assets/img/testimonial-avata-01.jpg'),
+                text: ' Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque, sit ut<br> my own schedule and choose the subject i<br> want to learn from the syllabus. Great<br> study portal for people like me.',
+                name: 'mimmo lace',
+                work: '/ Manager'
+            },
+            {
+                avatar: require('@/assets/img/testimonial-avata-03.jpg'),
+                text: 'I am free to learn at my own pace, follow<br>  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque, sit ut<br> want to learn from the syllabus. Great<br> study portal for people like me.',
+                name: 'michael red',
+                work: '/ Artist'
+            },
+            {
+                avatar: require('@/assets/img/testimonial-avata-04.jpg'),
+                text: 'I am free to learn at my own pace, follow<br> my own schedule and choose the subject i<br>  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque, sit ut<br> study portal for people like me.',
+                name: 'oliver jass',
+                work: '/ Author'
+            }
+        ],
+    }
+},
+methods: {
+    NextTestimonial() {
+        if (this.ActiveTestimonial == this.Testimonials.length - 1) {
+            this.ActiveTestimonial = 0;
+        } else {
+            this.ActiveTestimonial++;
+        }
+    },
+    PrecedentTestimonial() {
+        if (this.ActiveTestimonial == 0) {
+            this.ActiveTestimonial = this.Testimonials.length - 1;
+        } else {
+            this.ActiveTestimonial--;
+        }
+    }
+}
 }
 </script>
 
