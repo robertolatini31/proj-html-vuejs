c48edb57cc76f720a0f1f8488be64f02d06b5654
robertolatini31 <robertolatini31@gmail.com>
2022-05-17 15:09:45 +0200

    add fixed menu

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
 <template>
   <div id="app">
    
+   <div class="fixed_menu d-flex flex-column">
+     <font-awesome-icon class="fixed_icon" icon="fa-solid fa-ruler-combined" />
+     <font-awesome-icon class="fixed_icon" icon="fa-solid fa-life-ring" />
+     <font-awesome-icon class="fixed_icon" icon="fa-solid fa-book" />
+     <font-awesome-icon class="fixed_icon" icon="fa-solid fa-cart-shopping" />
+   </div>
+   <!-- /.fixed_menu -->
+
+
   <Header />
   <Main />
   <Footer />
@@ -41,7 +50,23 @@ export default {
 }
 
 
-
+.fixed_menu {
+  z-index: 10000;
+  position: fixed;
+  top: 130px;
+  right: -5px;
+  border-radius: 5px;
+  background-color: white;
+  box-shadow: 0 0 8px -4px black;
+  .fixed_icon:hover {
+    color: $Primary-color;
+  }
+  .fixed_icon {
+    padding: 0.8rem;
+    font-size: 1rem;
+    cursor: pointer;
+  }
+}
 
 
 </style>
--- src/main.js
+++ src/main.js
@@ -7,14 +7,14 @@ import App from './App.vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
 
 /* import specific icons 👇 QUI AGGIUNGI LE ICONE che vuoi usare in camelCase */
-import { faAngleDown, faBars, faArrowRightLong, faCheck, faMagnifyingGlass, faCartShopping, faSignal, faArrowUpLong} from '@fortawesome/free-solid-svg-icons'
+import { faAngleDown, faBars, faArrowRightLong, faCheck, faMagnifyingGlass, faCartShopping, faSignal, faArrowUpLong, faRulerCombined, faLifeRing, faBook} from '@fortawesome/free-solid-svg-icons'
 import {faFileLines, faUser, faHeart, faCalendar} from '@fortawesome/free-regular-svg-icons'
 import { faTwitter, faFacebookF, faInstagram, faLinkedin, faFacebookSquare } from '@fortawesome/free-brands-svg-icons'
 /* import font awesome icon component */
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 /* add icons to the library 👇 QUI AGGIUNGI IL NOME DELL'ICONA in camelCase*/
-library.add(faAngleDown, faTwitter, faFacebookF, faInstagram, faLinkedin, faBars, faArrowRightLong, faFileLines, faUser, faCheck, faMagnifyingGlass, faCartShopping, faHeart, faSignal, faCalendar, faFacebookSquare, faArrowUpLong) 
+library.add(faAngleDown, faTwitter, faFacebookF, faInstagram, faLinkedin, faBars, faArrowRightLong, faFileLines, faUser, faCheck, faMagnifyingGlass, faCartShopping, faHeart, faSignal, faCalendar, faFacebookSquare, faArrowUpLong, faRulerCombined, faLifeRing, faBook) 
 
 /* add font awesome icon component */
 Vue.component('font-awesome-icon', FontAwesomeIcon)
