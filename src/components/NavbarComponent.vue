18e64ba55592ba6b84e31292f1e8f7858feee1be
robertolatini31 <robertolatini31@gmail.com>
2022-05-17 13:13:52 +0200

    add mobile nav

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -26,7 +26,36 @@
        </div>
        <!-- /.menu -->
        <div class="social_nav">
-          <font-awesome-icon class="icon_social_nav change_menu" icon="fa-solid fa-bars" />
+
+        <div class="drop_container">
+            <font-awesome-icon @click="DropToggle" class="icon_social_nav change_menu" icon="fa-solid fa-bars" />
+
+            <div @mouseleave="DropOff" :class="ControlDrop ? 'd_show' : ''" class="change_menu_drop p-2 border">
+                    <ul class="d-flex flex-column p-0 m-0">
+                    <li class="d-flex align-items-center" v-for="HeaderMenuItem in HeaderMenu" :key="HeaderMenuItem.name">
+                    
+                        <div class="dropdown">
+                            <a class="dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">
+                                {{HeaderMenuItem.name}}
+                                <font-awesome-icon class="icon_arrow_down" icon="fa-solid fa-angle-down" />
+                            </a>
+
+                            <ul class="dropdown-menu">
+                                <li v-for="link in HeaderMenuItem.links" :key="link">
+                                    <a class="dropdown-item" href="#">{{link}}</a>
+                                </li>
+                            </ul>
+                        </div>
+
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <!-- /.drop_container -->
+        
+
+
+          
           <font-awesome-icon class="icon_social_nav d_none_sm" icon="fa-brands fa-twitter" />
           <font-awesome-icon class="icon_social_nav d_none_sm" icon="fa-brands fa-facebook-f" />
           <font-awesome-icon class="icon_social_nav d_none_sm" icon="fa-brands fa-instagram" />
@@ -98,8 +127,21 @@ data () {
         
         
        
-      ]
+      ],
+      ControlDrop: false,
     }
+  },
+  methods: {
+      DropToggle() {
+          if (this.ControlDrop == false) {
+              this.ControlDrop = true;
+          } else {
+               this.ControlDrop = false;
+          }
+      },
+      DropOff() {
+          this.ControlDrop = false;
+      }
   }
 }
 </script>
@@ -155,8 +197,53 @@ nav {
         color: $Dark-color;
       }
       .change_menu {
+          display: none;
+      }
+      .d_show {
+          display: block!important;
+      }
+      .change_menu_drop {
+        background-color: white;
+        
         display: none;
+        position: absolute;
+        top: 1.2rem;
+        right: 0;
+        border-radius: 5px;
       }    
+      .drop_container {
+          display: inline-block;
+          position: relative;
+        ul {
+        list-style: none;
+        margin: 0;
+        padding: 0;
+        column-gap: 3rem;
+        li:hover a, li:hover .icon_arrow_down {
+            color: $Primary-color;
+        }
+        li {
+          a, .icon_arrow_down {
+            color: $Secondary-color;
+          }
+          a {
+            text-decoration: none;
+            padding-right: 0.3rem;
+          }
+          .icon_arrow_down {
+            font-size: 0.7rem;
+          }
+          .dropdown-toggle::after {
+              content: none;
+          } 
+          .dropdown-menu > li > a:hover {
+                background-image: none;
+                background-color: $Primary-color;
+                color: white;
+            }
+        }
+      }
+      }
     }
   }
 @media screen and (max-width:"992px") {
