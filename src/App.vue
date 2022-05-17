83de9d0a7a34a93c768d996d113c063a33d86190
robertolatini31 <robertolatini31@gmail.com>
2022-05-17 16:18:13 +0200

    add fixed-menu component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,9 @@
 <template>
   <div id="app">
-   
-   <div class="fixed_menu d-flex flex-column">
-     <font-awesome-icon class="fixed_icon" icon="fa-solid fa-ruler-combined" />
-     <font-awesome-icon class="fixed_icon" icon="fa-solid fa-life-ring" />
-     <font-awesome-icon class="fixed_icon" icon="fa-solid fa-book" />
-     <font-awesome-icon class="fixed_icon" icon="fa-solid fa-cart-shopping" />
-   </div>
-   <!-- /.fixed_menu -->
-
-
+  <FixedMenu />
   <Header />
   <Main />
   <Footer />
-  
   </div>
 </template>
 
@@ -21,12 +11,14 @@
 import Header from '@/components/SiteHeader.vue'
 import Main from '@/components/SiteMain.vue'
 import Footer from '@/components/SiteFooter.vue'
+import FixedMenu from '@/components/FixedMenuComponent.vue'
 export default {
   name: 'App',
   components: {
     Header,
     Main,
     Footer,
+    FixedMenu,
   },
   
   
@@ -48,25 +40,5 @@ export default {
 .py_4 {
   padding: 4rem 0;
 }
-
-
-.fixed_menu {
-  z-index: 10000;
-  position: fixed;
-  top: 130px;
-  right: -5px;
-  border-radius: 5px;
-  background-color: white;
-  box-shadow: 0 0 8px -4px black;
-  .fixed_icon:hover {
-    color: $Primary-color;
-  }
-  .fixed_icon {
-    padding: 0.8rem;
-    font-size: 1rem;
-    cursor: pointer;
-  }
-}
-
 
 </style>
---
+++ src/components/FixedMenuComponent.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="fixed_menu d-flex flex-column">
+     <font-awesome-icon class="fixed_icon" icon="fa-solid fa-ruler-combined" />
+     <font-awesome-icon class="fixed_icon" icon="fa-solid fa-life-ring" />
+     <font-awesome-icon class="fixed_icon" icon="fa-solid fa-book" />
+     <font-awesome-icon class="fixed_icon" icon="fa-solid fa-cart-shopping" />
+   </div>
+   <!-- /.fixed_menu -->
+</template>
+
+<script>
+export default {
+name: 'FixedMenuComponent',
+}
+</script>
+
+<style lang="scss" scoped>
+.fixed_menu {
+  z-index: 10000;
+  position: fixed;
+  top: 130px;
+  right: -5px;
+  border-radius: 5px;
+  background-color: white;
+  box-shadow: 0 0 8px -4px black;
+  .fixed_icon:hover {
+    color: $Primary-color;
+  }
+  .fixed_icon {
+      transition: all .2s;
+    padding: 0.8rem;
+    font-size: 1rem;
+    cursor: pointer;
+  }
+}
+
+</style>
